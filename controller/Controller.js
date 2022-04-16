ae8aec466836c91c19530de9e65b8d502def5f35
Ekam Preet Singh <ekampreet.singh6862@gmail.com>
2022-04-16 20:12:55 +0530

    Adding comments to Assignment 1 -> Q1

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,23 +1,30 @@
+// Defining empty array
 let users = [];
 
+// GET method function
 const getUsers = (req, res) => {
+  // Sending user details
   res.send(users);
 };
 
+// POST method function
 const createUser = (req, res) => {
+  // Creating a random name
   let text = "";
   const possible =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-
+  // Pushing it into users array
   users.push({ ...users, text });
 
+  // Displaying that user has been added
   res.send(`User added to the database!`);
 };
 
+// Exporting middleware to Route.js
 module.exports = {
   getUsers,
   createUser,
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+// Initialzing modules
 const express = require("express");
 const app = express();
-
+// Setting PORT
 const PORT = 3000;
+// Requiring routes from Route.js from routes folder
 const user = require("./routes/Route");
 
+// Defining Home Route
 app.get("/", (req, res) => res.send("Welcome to the Home Page"));
 
+// Defining user route
 app.use("/users", user);
 
+// Starting app on  http://localhost:3000
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
